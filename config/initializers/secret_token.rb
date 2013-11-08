8533161f3a9040e7d0e90f5aa310c0fbb0a8da7e
Michael Rapadas <mike@mrap.me>
2013-11-08 01:32:36 -0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Scout::Application.config.secret_key_base = '37926799941da72757a3de2fa1aeb14531c2b50bdcca9245d79b119ebd81d5c9f1cf9fca6bd56920b7bbb1813a4c80b8b3c744a114c38c559be20585b0e27695'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Scout::Application.config.session_store :cookie_store, key: '_scout_session'
