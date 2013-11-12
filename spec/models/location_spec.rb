f3dd2a2f88b2d4c6c9f2be49c6f4e825d132d98f
Michael Rapadas <mike@mrap.me>
2013-11-12 00:10:44 -0800

    Cleans up specs

--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -1,11 +1,12 @@
 require 'spec_helper'
 
 describe Location do
+
   it { should have_fields :coordinates, :created_at }
   it { should validate_presence_of :coordinates }
 
   describe "finding nearby locations" do
-    before do
+     before do
       @my_location = build(:location, coordinates: { latitude: 37.781127, longitude: -122.417102 })
       @nearby_location = create(:location, coordinates: { latitude: 37.781128, longitude: -122.417103 })
       @far_location = create(:location, coordinates: { latitude: -37.781128, longitude: 122.417103 })
@@ -21,6 +22,14 @@ describe Location do
         results = @my_location.nearby_with_distance(kilometers: 1).to_a
         results.should include @nearby_location
         results.should_not include @far_location
+      end
+    end
+
+    describe ".nearby" do
+      it "should return locations closest to furthest" do
+        locations_near_me = @my_location.nearby.to_a
+        locations_near_me.first.should eq @nearby_location
+        locations_near_me.last.should eq @far_location
       end
     end
   end
