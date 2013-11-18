5ac04dc9d6ded4f33fcc4999387f425738e6d1d3
Michael Rapadas <mike@mrap.me>
2013-11-17 19:59:55 -0800

    adds :score field to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User
   has_and_belongs_to_many :helpful_posts, class_name: "Post", inverse_of: :helped_users
 
   field :username, type: String
+  field :score,    type: Integer, default: 0
 
   validates_presence_of :username
   validates_uniqueness_of :username
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,6 +5,7 @@ describe User do
   it { should have_many :posts }
   it { should have_and_belong_to_many(:helpful_posts).of_type(Post).as_inverse_of(:helped_users) }
   it { should have_field :username }
+  it { should have_field(:score).of_type(Integer).with_default_value_of(0) }
   it { should validate_presence_of :username }
   it { should validate_uniqueness_of :username }
 
