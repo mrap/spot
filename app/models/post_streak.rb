702ed1b3f93ef076e976219f070f3b9d9fc37b55
Michael Rapadas <mike@mrap.me>
2013-11-20 00:59:49 -0800

    Adds :expiration_time and :expired
    - post_streak does not expire itself
    - expire interval constant in app_constants.rb

--- app/models/post_streak.rb
+++ app/models/post_streak.rb
@@ -1,10 +1,18 @@
 class PostStreak
   include Mongoid::Document
+  include Mongoid::Timestamps
 
   belongs_to :place
   has_many :posts
 
+  field :expired,         type: Boolean,       default: false
+  field :expiration_time, type: ActiveSupport::TimeWithZone,  default: lambda{ PostStreak.new_expiration_time }
+
   validate :validate_minimum_posts_count
+
+  def self.new_expiration_time
+    Time.zone.now.advance(seconds: POST_STREAK_EXPIRATION_INTERVAL)
+  end
 
   private
 
--- spec/models/post_streak_spec.rb
+++ spec/models/post_streak_spec.rb
@@ -4,9 +4,20 @@ describe PostStreak do
 
   it { should belong_to :place }
   it { should have_many :posts }
+  it { should have_field :created_at }
+  it { should have_field :updated_at }
+  it { should have_field(:expired).of_type(Boolean).with_default_value_of(false) }
+  it { should have_field(:expiration_time).of_type(ActiveSupport::TimeWithZone) }
 
   it "should not allow post_streaks that do not have the minimum amount of posts" do
     expect{ create(:post_streak, posts_count: MINIMUM_POSTS_PER_STREAK - 1) }.to raise_error
     expect{ create(:post_streak) }.to change{ PostStreak.count }.by(1)
+  end
+
+  # Instance Specs
+
+  subject(:post_streak) { create(:post_streak) }
+  it "should have correct expiration_time" do
+    post_streak.expiration_time.round.should eq PostStreak.new_expiration_time.round
   end
 end
