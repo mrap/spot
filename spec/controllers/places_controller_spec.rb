1d8a02fd9cb4ec17b447f7b8e210c1901ebd398b
Michael Rapadas <mike@mrap.me>
2013-11-22 13:26:20 -0800

    sets spec for vcr to record new_episodes

--- spec/controllers/places_controller_spec.rb
+++ spec/controllers/places_controller_spec.rb
@@ -4,16 +4,16 @@ describe PlacesController do
   let(:coordinates) { build(:chipotle_factual_place).coordinates }
   let(:latitude)    { coordinates.latitude }
   let(:longitude)    { coordinates.longitude }
-  describe "GET #nearby", :vcr => { record: :new_episodes } do
+  describe "GET #nearby" do
     # Required url params: [:longitude, :latitude]
     # Optional url params: [:radius, :search_terms]
-    context "with latitude and longitude params" do
+    context "with latitude and longitude params", :vcr do
       it "is successful" do
         get :nearby, latitude: latitude, longitude: longitude
         expect(response).to be_success
       end
     end
-    context "when not :latitude and :longitude not provided " do
+    context "when not :latitude and :longitude not provided ", :vcr do
       it "is successful" do
         get :nearby
         response.status.should eq 400
