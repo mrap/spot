2ac8aa72d9beaeacdbc3669ed5981123e884e80b
Michael Rapadas <mike@mrap.me>
2013-11-23 17:29:37 -0800

    Removes rewards from User

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,6 @@ class User
 
   has_many :posts, inverse_of: :author
   has_and_belongs_to_many :helpful_posts, class_name: "Post", inverse_of: :helped_users
-  has_many :rewards_given, class_name: "Reward", inverse_of: :giver, dependent: :destroy
-  has_many :rewards_received, class_name: "Reward", inverse_of: :receiver, dependent: :destroy
 
   field :username, type: String
   field :score,    type: Integer, default: 0
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,8 +4,6 @@ describe User do
 
   it { should have_many(:posts).as_inverse_of(:author) }
   it { should have_and_belong_to_many(:helpful_posts).of_type(Post).as_inverse_of(:helped_users) }
-  it { should have_many(:rewards_given).as_inverse_of(:giver).with_dependent(:destroy) }
-  it { should have_many(:rewards_received).as_inverse_of(:receiver).with_dependent(:destroy) }
   it { should have_field :username }
   it { should have_field(:score).of_type(Integer).with_default_value_of(0) }
   it { should validate_presence_of :username }
