d9e0f60b1bd421eb352507e5be171620f60df595
Michael Rapadas <mike@mrap.me>
2013-11-26 01:02:48 -0800

    Refactors set_new_query into places#nearby action and corrects ivar.
    * removes set_new_query before_action -> integrates into #nearby
      PlaceQuery is specific to #nearby.
      Also, PlaceQuery's .search_nearby_coordinates explicity requires coordinates
      as a parameter.
    * ivar @place_query -> @places
      @place or @places is the primary value that Places Controller provides
      to views.  Keep the standard.

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,7 +1,6 @@
 class PlacesController < ApplicationController
 
   before_action :require_coordinates!, only: [:nearby]
-  before_action :set_new_query, only: [:nearby]
 
   # Returns a list of places nearby coordinates. Places ordered by distance.
   #
@@ -10,16 +9,16 @@ class PlacesController < ApplicationController
   # Options  => :search_terms, :radius(in meters)
   def nearby
     require_coordinates!
-    @place_query.search_nearby_coordinates(@coordinates, radius: params[:radius], search_terms: params[:search_terms])
-    render json: @place_query.results
+    place_query = PlaceQuery.new
+    place_query.search_nearby_coordinates(@coordinates, radius: params[:radius], search_terms: params[:search_terms])
+    @places = place_query.results
+    render json: @places
   end
 
   private
 
-    def set_new_query
-      @place_query = PlaceQuery.new
-    end
-
+    # Requires both `longitude` and `latitude` url params.
+    # Else, renders nothing with `bad request` status code.
     def require_coordinates!
       if params[:longitude] && params[:latitude]
         @coordinates = Coordinates.new(params[:longitude], params[:latitude])
