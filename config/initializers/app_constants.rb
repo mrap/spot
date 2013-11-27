ef9947533917bd3979e9dab462dc3bc386806669
Michael Rapadas <mike@mrap.me>
2013-11-26 16:57:03 -0800

    Renames AppConstants -> AppSettings and adds documentation.
    Documents how to use AppSettings. Includes a detailed example.

--- config/initializers/app_constants.rb
+++ config/initializers/app_constants.rb
@@ -1,12 +1,31 @@
-module AppConstants
+#
+# Centralized app configuration settings.
+#
+# This module encapsulates hashes with configuration keys => values
+#
+# Convention:
+# Only the model should access its own AppSettings hash. 
+# Everywhere else should use it from the model.
+#   
+#   # AppSettings.rb
+#     MODEL = {
+#       config: config_value
+#     }
+#
+#   # Model.rb
+#   class Model
+#     CONFIG = AppSettings::MODEL[:config]
+#     ...
+#   end
+#
+#   # Anywhere else outside of the Model.rb 
+#   # that needs to use the config (specs, controller, etc)
+#     Model::CONFIG
+#
+module AppSettings
+
   QUERY = {
     default_search_radius: 5000 # in meters
   }
-  REWARD = {
-    base_score: 10
-  }
-  POST_STREAK = {
-    minimum_post_count: 3,
-    expiration_interval: 600
-  }
+
 end
