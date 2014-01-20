7463aae9a1f0cc8639e3efa06462ebe64f3cdec0
Michael Rapadas <mike@mrap.me>
2014-01-19 18:37:23 -0800

    Cleans up spec to use let.

--- spec/requests/places/get_a_place_spec.rb
+++ spec/requests/places/get_a_place_spec.rb
@@ -9,27 +9,29 @@ describe "Requesting for a place JSON reference" do
       get api_v1_place_path(place.id), nil, set_token_auth_with_user
     end
 
+    let(:data) { json['data'] }
+
     it "should have an id" do
-      json['data']['id'].should eq place.id.to_s
-      json['data'].should_not have_key :_id
+      data['id'].should eq place.id.to_s
+      data.should_not have_key :_id
     end
 
     it "should have a name" do
-      json['data']['name'].should eq place.name
+      data['name'].should eq place.name
     end
 
     it "should have latitude and longitude but not `coordinates`" do
-      json['data']['longitude'].should eq place.longitude
-      json['data']['latitude'].should eq place.latitude
-      json['data'].should_not have_key 'coordinates'
+      data['longitude'].should eq place.longitude
+      data['latitude'].should eq place.latitude
+      data.should_not have_key 'coordinates'
     end
 
     it "should have address info" do
-      json['data']['address'].should eq place.address
+      data['address'].should eq place.address
     end
 
     it "should not have _keywords (from mongoid full text search)" do
-      json['data'].should_not have_key "_keywords"
+      data.should_not have_key "_keywords"
     end
   end
 
