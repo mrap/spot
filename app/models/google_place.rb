7a267534772bdc7db71fc375604c067d7d059905
Michael Rapadas <mike@mrap.me>
2014-05-31 01:11:58 -0700

    GooglePlace#places_with_coordinates creates GooglePlace objects.
    * Adds method GooglePlace#create_from_api_ref(ref)
    * Adds method GooglePlace#find_or_create_from_api_ref(ref)

--- app/models/google_place.rb
+++ app/models/google_place.rb
@@ -1,9 +1,30 @@
-class GooglePlace
-  # include Mongoid::Document
+class GooglePlace < Place
+
+  field :google_id
+  validates :google_id, presence: true, uniqueness: true
 
   def self.places_with_coordinates(coordinates)
     return [] unless coordinates.latitude && coordinates.longitude
-    GooglePlacesClient.spots(coordinates.latitude, coordinates.longitude, {rankby: 'distance', keyword: '*'} )
+    places = GooglePlacesClient.spots(coordinates.latitude, coordinates.longitude, {rankby: 'distance', keyword: '*'} )
+    places.collect { |place_ref| GooglePlace.find_or_create_from_api_ref(place_ref) }
+  end
+
+  def self.create_from_api_ref(ref)
+    return nil unless ref
+
+    place             = GooglePlace.new
+    place.google_id   = ref.id if ref.id
+    place.name        = ref.name if ref.name
+    place.address     = ref.vicinity if ref.vicinity
+    place.coordinates = Coordinates.new_with_lat_long(ref.lat, ref.lng)
+
+    return place if place.save
+    raise "Place was not created!"
+  end
+
+  def self.find_or_create_from_api_ref(ref)
+    return nil unless ref
+    GooglePlace.where(google_id: ref.id).first || GooglePlace.create_from_api_ref(ref)
   end
 
 end
--- spec/models/google_place_spec.rb
+++ spec/models/google_place_spec.rb
@@ -1,14 +1,30 @@
 require 'spec_helper'
 
 describe GooglePlace do
+  it { should have_field :google_id }
+  it { should validate_presence_of :google_id }
+  it { should validate_uniqueness_of :google_id }
 
-  describe "#places_with_coordinates" do
+  describe "creating from a api ref" do
     let(:chipotle_coordinates) { Coordinates.new_with_lat_long(37.6717, -122.464216) }
+    let(:api_results)          { GooglePlacesClient.spots(chipotle_coordinates.latitude, chipotle_coordinates.longitude) }
+    let(:api_ref)              { api_results.first }
+    subject(:place)            { GooglePlace.create_from_api_ref api_ref }
 
-    it "should return an array of places" do
-      result = GooglePlace.places_with_coordinates(chipotle_coordinates)
-      result.should have_at_least(1).items
-    end
+    its(:name)        { should_not be_nil }
+    its(:address)     { should_not be_nil }
+    its(:coordinates) { should_not be_nil }
   end
 
+  describe "#places_with_coordinates" do
+    let(:chipotle_coordinates)        { Coordinates.new_with_lat_long(37.6717, -122.464216) }
+    subject(:places_with_coordinates) { GooglePlace.places_with_coordinates(chipotle_coordinates) }
+
+    it { should have_at_least(1).items }
+
+    it "should create the places in the database" do
+      places = places_with_coordinates
+      GooglePlace.count.should eq places.count
+    end
+  end
 end
